9eedf13f38c1338f9ea4d38e801cc6048a49112f
marsik-ms <79077490+marsik-ms@users.noreply.github.com>
2023-06-24 16:40:50 +0300

    Initial Commit

---
+++ ContactListUI/ContactListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListUIApp.swift
+//  ContactListUI
+//
+//  Created by Stanislav Perepechin on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
